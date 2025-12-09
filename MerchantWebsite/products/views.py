a9773d33733b0d2ed1c6aa98785fcb0ec458e220
Hassan Ali <n01728997@students.humber.ca>
2025-12-08 23:41:12 -0500

    Added  error handling to models.py and views.py

--- MerchantWebsite/products/views.py
+++ MerchantWebsite/products/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+try: 
+    product= Products.objects.get(id=product_id)
+except Products.DoesNotExist:
+    messages.error(request,"Products not found")
+    return redirect("products")
+
+
+try:
+    cart_item created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product,
+        )
+    cart_item.quantity += 1
+    cart_item.full_clean()
+    cart_item.save()
+
+except Exception as e:
+        messages.error(request, "Error adding item to cart")
+    return redirect("products")
+
+return redirect("cart")
